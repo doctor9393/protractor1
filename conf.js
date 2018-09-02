2a44ae1ab68f83c601ee8c990dcb24342c5e2489
doctor9393 <nazar.milyan@gmail.com>
2018-09-02 13:24:55 +0300

    two browsers

--- conf.js
+++ conf.js
@@ -2,8 +2,9 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['spec.js'],
-    capabilities:{
-        browserName: "chrome"
-
-    }
+    multiCapabilities: [{
+        'browserName': 'firefox'
+      }, {
+        'browserName': 'chrome'
+      }]
 }
--- spec.js
+++ spec.js
@@ -1,10 +1,6 @@
-describe ('Protractor Demo App', function(){
-    it ('should add one and two', function(){
+describe('Protractor Demo App', function(){
+    it('should have a title', function (){
         browser.get('http://juliemr.github.io/protractor-demo/');
-        element(by.model('first')).sendKeys(1);
-        element(by.model('second')).sendKeys(2);
-        element (by.id('gobutton')).click();
-        expect(element(by.binding('latest')).getText()).
-        expect(element(by.binding('latest')).getText()).toEqual('5');
-    });
-});
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    })
+})
