ffabb1eefaf9f1ae7b84f68901503f87dc329a60
doctor9393 <nazar.milyan@gmail.com>
2018-09-02 13:46:43 +0300

    commit with on prepare

--- conf.js
+++ conf.js
@@ -2,9 +2,14 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['spec.js'],
+    onPrepare: function() {
+        console.log("--------------------------Message before start of SERVER----------------------------------------")
+
+    },
     multiCapabilities: [{
         'browserName': 'firefox'
       }, {
         'browserName': 'chrome'
       }]
+
 }
